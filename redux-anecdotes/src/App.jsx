910d345af6a1b3f60590957564142d20dc89ed6d
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-06-19 00:11:20 -0600

    6.3: Anécdotas, paso 1 - Completado

---
+++ redux-anecdotes/src/App.jsx
@@ -0,0 +1,36 @@
+import { useSelector, useDispatch } from 'react-redux'
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = id => {
+    dispatch({ type: 'VOTE', payload: { id } })
+  }
+
+  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes)
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {sortedAnecdotes.map(anecdote => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+      <h2>create new</h2>
+      <form>
+        <div>
+          <input />
+        </div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
