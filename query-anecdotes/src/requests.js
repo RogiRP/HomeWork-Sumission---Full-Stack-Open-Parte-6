e20cdaaabb80d56c09ddf04662d8d61fba34dc1a
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-06-26 20:15:16 -0600

    Ejercicio 6.20 - Completado

---
+++ query-anecdotes/src/requests.js
@@ -0,0 +1,9 @@
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+export const getAnecdotes = async () => {
+  const response = await fetch(baseUrl)
+  if (!response.ok) {
+    throw new Error('Error fetching anecdotes')
+  }
+  return response.json()
+}
