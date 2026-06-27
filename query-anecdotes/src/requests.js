7df63dc9546fa643745a1d20b50e608a7bf9431b
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-06-26 20:17:43 -0600

    Ejercicio 6.21 - Completado

--- query-anecdotes/src/requests.js
+++ query-anecdotes/src/requests.js
@@ -7,3 +7,12 @@ export const getAnecdotes = async () => {
   }
   return response.json()
 }
+
+export const createAnecdote = async (content) => {
+  const response = await fetch(baseUrl, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ content, votes: 0 })
+  })
+  return response.json()
+}
